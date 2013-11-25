fb75faca63a6fa697b394a87c1012c633eb8cd35
Robin <i12017@hb.dhbw-stuttgart.de>
2013-11-25 15:17:55 +0100

    Erweiterung Statements

---
+++ compilerbau/src/de/dhbw/blaaah/statements/UpdateStatement.java
@@ -0,0 +1,11 @@
+package de.dhbw.blaaah.statements;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Robin Lang
+ * Date: 25.11.13
+ * Time: 15:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class UpdateStatement {
+}
