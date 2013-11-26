f40f4a59f083f04a1fa5e2dac7342a840b0f53b2
Alexander Hausmann <hausmann.alex@gmail.com>
2013-11-26 16:17:40 +0100

    CUP und JFlex-Binärdateien eingefügt - Parsergeneration angepasst

--- compilerbau/parser/makeparser.sh
+++ compilerbau/parser/makeparser.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+JFLEX_JAR=../bin/JFlex.jar
+CUP_JAR=../bin/java-cup-11a.jar
+JAVA=java
+
 DEST_DIR=../src/de/dhbw/blaaah/parser
 PARSER_NAME=MiniSqlParser
 
-cup -destdir $DEST_DIR -parser $PARSER_NAME minisql.cup
+# Java-Klassen erzeugen
 
-jflex -d $DEST_DIR minisql.flex
+$JAVA -jar $CUP_JAR -destdir $DEST_DIR -parser $PARSER_NAME minisql.cup
+
+$JAVA -jar $JFLEX_JAR -d $DEST_DIR minisql.flex
