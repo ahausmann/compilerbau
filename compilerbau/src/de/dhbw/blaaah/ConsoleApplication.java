84f8d507db501ede60864d4049fd3e278ab1700b
Alexander Hausmann <hausmann.alex@gmail.com>
2013-11-27 13:22:45 +0100

    Konsolenapplikation begonnen

---
+++ compilerbau/src/de/dhbw/blaaah/ConsoleApplication.java
@@ -0,0 +1,30 @@
+package de.dhbw.blaaah;
+
+import de.dhbw.blaaah.database.csv.CSVDatabase;
+
+/**
+ * Diese Klasse stellt eine simple Konsolenanwendung bereit, die sich mit einer Datenbank verbinden
+ * kann und dort Befehle ausführen kann.
+ */
+public class ConsoleApplication {
+
+    private final Database database;
+
+    public ConsoleApplication(String dbName) {
+        database = new CSVDatabase("./" + dbName);
+
+        while (true) {
+            // Befehle aus System.in lesen und mit Parser parsen
+            // evtl. bei "exit" beenden
+
+            // Statements dann ausführen
+        }
+
+        // Speichern
+        database.save();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
