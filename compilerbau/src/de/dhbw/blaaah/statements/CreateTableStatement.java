609dcbd664929dec360ea52fd243234374a23086
Robin <i12017@hb.dhbw-stuttgart.de>
2013-12-01 19:34:22 +0100

    CreateTableStatement und UpdateStatement impl.

--- compilerbau/src/de/dhbw/blaaah/statements/CreateTableStatement.java
+++ compilerbau/src/de/dhbw/blaaah/statements/CreateTableStatement.java
@@ -3,7 +3,13 @@ package de.dhbw.blaaah.statements;
 import de.dhbw.blaaah.Database;
 import de.dhbw.blaaah.Result;
 import de.dhbw.blaaah.Statement;
+import de.dhbw.blaaah.WhereCondition;
+import de.dhbw.blaaah.database.ColumnType;
+import de.dhbw.blaaah.database.TableDefinition;
+import de.dhbw.blaaah.exceptions.ColumnDefinedException;
 import de.dhbw.blaaah.exceptions.DatabaseException;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,10 +17,33 @@ import de.dhbw.blaaah.exceptions.DatabaseException;
  * Date: 25.11.13
  * Time: 15:16
  * To change this template use File | Settings | File Templates.
+ *
+ * Create-Table-Statement: CREATE TABLE tableName columns
  */
 public class CreateTableStatement implements Statement {
+
+    List<String> columns;
+    String tableName;
+    WhereCondition whereCondition;
+
+    public CreateTableStatement(List<String> columns, String tableName, WhereCondition whereCondition){
+        this.columns = columns;
+        this.tableName = tableName;
+        this.whereCondition = whereCondition;
+    }
     @Override
     public Result execute(Database database) throws DatabaseException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        TableDefinition tableDefinition = new TableDefinition(tableName);
+        ColumnType cT = ColumnType.TEXT;
+
+        for(int i = 0; i < columns.size(); i++){
+            try {
+                tableDefinition.addColumn(columns.get(i), cT);
+            } catch (ColumnDefinedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return database.createTable(tableDefinition);
     }
 }
--- compilerbau/src/de/dhbw/blaaah/statements/UpdateStatement.java
+++ compilerbau/src/de/dhbw/blaaah/statements/UpdateStatement.java
@@ -1,11 +1,43 @@
 package de.dhbw.blaaah.statements;
 
+import de.dhbw.blaaah.*;
+import de.dhbw.blaaah.database.ColumnDefinition;
+import de.dhbw.blaaah.database.TableDefinition;
+import de.dhbw.blaaah.exceptions.DatabaseException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Robin Lang
  * Date: 25.11.13
  * Time: 15:17
  * To change this template use File | Settings | File Templates.
+ *
+ * Update-Statement: UPDATE tableName SET column = value WHERE whereCondition
  */
-public class UpdateStatement {
+public class UpdateStatement implements Statement {
+
+    private String tableName;
+    private String columnName;
+    private Object value;
+    private WhereCondition whereCondition;
+
+    public UpdateStatement(String tableName, String columnName, Object value, WhereCondition whereCondition){
+        this.tableName = tableName;
+        this.columnName = columnName;
+        this.value = value;
+        this.whereCondition = whereCondition;
+    }
+
+    @Override
+    public Result execute(Database database) throws DatabaseException {
+        Table table = database.getTable(tableName);
+
+        for(Row row : table.getRows()){
+            if(whereCondition.matches(row)){
+                table.changeCell(row.getRowIndex(), columnName, value);
+            }
+        }
+
+        return table.getDatabase().getResultFactory().createSuccessResult();
+    }
 }
